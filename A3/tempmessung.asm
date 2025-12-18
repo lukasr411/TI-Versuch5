95957b5c8b4f056aa8458a709ab861236cdd9a51
Lukas Ritter <lukas.ritter06@gmail.com>
2025-12-18 14:03:53 +0100

    tempmessung neu

--- A3/tempmessung.asm
+++ A3/tempmessung.asm
@@ -1,30 +1,35 @@
 #! mrasm
 
-.EQU last_value 0x00    ; letzter Messwert RAM Adresse
-.EQU last_state 0x01    ; letzter Comparator-state RAM Adresse
+.EQU last_value 0        ; letzter Messwert RAM Adresse
+.EQU last_state 1        ; letzter Comparator-state RAM Adresse
 
-.EQU bit4_mask 0x10     ; Konstante um 4. Bit zu masken
-.EQU platine 0xF1       ; Adresse um DAC2 und Comparator anzusprechen
-.ORG 0x20               ; Programm startpunkt 
+.EQU buffer_register 2   ; Register zwischenspeichern
+.EQU bit4_mask 16        ; Konstante um 4. Bit zu masken
+.EQU platine 241         ; Adresse um DAC2 und Comparator anzusprechen
+.ORG 0x20                ; Programm startpunkt 
 
 ;---------------------------------------------------------------------------------
 
 TEMPMESS:
-    LD R0, last_value
-    LD R1, last_state
+    LD R0, (last_value)
+    LD R1, (last_state)
 
 LOOP:
-    ST platine, R0      ; DAC Wert für R0 prüfen
+    ST (platine), R0            ; DAC Wert für R0 prüfen
+    ST (buffer_register), R0    ; R0 zwischenspeichern um R0 für AND vergleich zu nutzen
+    
+    LD R0, bit4_mask            ; R0 = bitmaske
+    LD R2, (platine)            ; Byte laden um Status-Bit zu lesen
+    AND R2, R0                  ; R2 = R2 AND R0 
 
-    LD R2, platine      ; Byte laden um Status-Bit zu lesen
-    AND R2, bit4_mask   ; Bit 4 maskieren
-
+    LD R0, (buffer_register)    ; Vorherigen R0 Wert aus zwischenspeicher holen
+    
     CMP R2, R1          
-    JZ NO_SWITCH        ; Case: state hat sich nicht geändert, also +/- 1
+    JZS NO_SWITCH                ; Case: state hat sich nicht geändert, also +/- 1
 
 SWITCH:                 ; Case: state hat sich geändert, also mache letzten Schritt rückgängig
     CMP R1, bit4_mask   ; und speicher R0
-    JZ INC_REVERT
+    JZS INC_REVERT
     
     INC R0              
     JMP STORE
@@ -35,7 +40,7 @@ INC_REVERT:
 
 NO_SWITCH: 
     CMP R2, 0x10        
-    JZ INC_VALUE        ; Case: Inkrementiere, da U_dac < U_sens
+    JZS INC_VALUE        ; Case: Inkrementiere, da U_dac < U_sens
     
     DEC R0              ; Case: Dekrementiere, da U_dac >= U_sens
     JMP UPDATE
@@ -48,7 +53,7 @@ UPDATE:
     JMP LOOP
 
 STORE:  
-    ST last_value, R0   ; Speichern des finalen R0 Wertes in Adresse 0x00
-    ST last_state, R2
+    ST (last_value), R0   ; Speichern des finalen R0 Wertes in Adresse 0x00
+    ST (last_state), R2
     
     RET
